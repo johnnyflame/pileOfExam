0b59e94d19348bb41f19c2ed73fc5bb2fc6b2e86
johnnyflame <johnnyflamelee@gmail.com>
2016-04-24 15:10:53 +1200

    Hi!

---
+++ src/EP.java
@@ -0,0 +1,34 @@
+
+/**
+ *
+ * @author Johnny Flame Lee and Frida Israelsson
+ */
+public class EP implements ExamPile{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public void load (List<Integer> items){
+        
+    }
+    
+    public int peek(){
+        
+    }
+    
+    public int mark(int depth, int value){
+        
+    }
+    public void delay(int count){
+        
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
