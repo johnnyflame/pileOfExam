940ebd2a36bcd7df9b65573259badf27e153fa7f
johnnyflame <johnnyflamelee@gmail.com>
2016-04-24 16:44:44 +1200

    Penis

--- src/EP.java
+++ src/EP.java
@@ -9,17 +9,40 @@ public class EP implements ExamPile{
      * @param args the command line arguments
      */
     
+    /**
+     * Initializes the pile of exams to consist of
+     * the contents of the list provided (the elements of the list represent the exams
+     * in order from top to bottom of the pile).
+     * @param items
+     */
     public void load (List<Integer> items){
         
     }
     
+    /**
+     * 
+     * @return s the value at the top of the pile
+     */
     public int peek(){
         
     }
-    
+    /**
+     * Returns the given value from the top
+     * section of the pile, as specified by depth, and removes it from the pile. Returns
+     * -1 if the value is not found.
+     * @param depth
+     * @param value
+     * @return 
+     */
     public int mark(int depth, int value){
         
     }
+    
+    /**
+     * Moves count values from the top of the pile to the
+     * bottom of the pile.
+     * @param count 
+     */
     public void delay(int count){
         
     }
